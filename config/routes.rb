10de27b7c6252b7195f7532ea54456189bad5cd3
ahaha-2552 <aratanboo@yahoo.co.jp>
2020-07-05 11:36:01 +0900

    Revert "Merge pull request #4 from ahaha-2552/メッセージ送信機能"
    This reverts commit c98e5421edb0dcc341ca03873ca83ad60cdc4fa1, reversing
    changes made to deb5e4b54fd4c71a5982ca4f1df2b1e016f47a32.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,4 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
 end
